a5d1875cb81d76363ba0d9fcbf8770a2e88b055d
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-07-19 09:34:45 +0700

    setup route menggunakan jsx

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,22 +1,35 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Details from "./pages/Details";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/details",
-    element: <Details />,
-  },
-]);
+// Mendefinisikan route
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Home />,
+//   },
+//   {
+//     path: "/details",
+//     element: <Details />,
+//   },
+// ]);
 
 function App() {
   return (
     <div className="flex justify-center items-center">
-      <RouterProvider router={router} />
+      {/* react provider for object */}
+      {/* <RouterProvider router={router} /> */}
+
+      {/* Mendefinisikan route menggunakan jsx */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
     </div>
   );
 }
